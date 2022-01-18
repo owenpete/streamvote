e1a346e3043699d9418d87fc6a43aace21b652c3
Owen Peterson <owengpete@gmail.com>
2022-01-17 21:30:34 -0600

    add timer controls

--- components/Timer.tsx
+++ components/Timer.tsx
@@ -9,6 +9,20 @@ const Timer = (props: Props) =>{
   return (
     <div className="timer">
       5:00
+      {
+        props.isRunning?
+        <input 
+          type='button'
+          value='Stop'
+          className='timer__stop timer__button'
+        />
+        :
+        <input 
+          type='button'
+          value='Start'
+          className='timer__start timer__button'
+        />
+      }
     </div>
   );
 }
