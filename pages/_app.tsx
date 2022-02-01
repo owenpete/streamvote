b7efa22888f69d22a89981b116154a1fdba3e6ac
Owen Peterson <owengpete@gmail.com>
2022-01-31 23:55:54 -0600

    add NProgrss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,7 @@
+import Router from "next/router";
+import NProgress from "nprogress";
+import "../styles/nprogress.scss";
+
 import '../styles/globals.css';
 import '../styles/index.scss';
 import '../styles/main.scss';
@@ -10,6 +14,11 @@ import '../styles/voteControls.scss';
 import '../styles/newCategoryPopup.scss';
 import '../styles/newChannelPopup.scss';
 import type { AppProps } from 'next/app'
+
+NProgress.configure({ showSpinner: false });
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
