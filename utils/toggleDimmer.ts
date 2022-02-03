6d3d2d9b6793d5fd694b77c2470e758479103fa5
Owen Peterson <owengpete@gmail.com>
2022-02-02 22:01:28 -0600

    fix error where users could click through the dimmer while it was activated

--- utils/toggleDimmer.ts
+++ utils/toggleDimmer.ts
@@ -2,10 +2,10 @@ const toggleDimmer = (isEnabled: boolean) =>{
   const dimmerElement: HTMLElement = document.getElementById('dimmer')!;
     if(isEnabled){
       dimmerElement.style.backgroundColor='hsla(0, 0%, 0%, 35%)';
-      dimmerElement.style.pointerEvents='none';
+      dimmerElement.style.pointerEvents='auto';
     }else{
       dimmerElement.style.backgroundColor='';
-      dimmerElement.style.pointerEvents='';
+      dimmerElement.style.pointerEvents='none';
     }
 }
 
