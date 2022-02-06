4fd50999f4c75966911c59eb5f131369e5dc9fae
Owen Peterson <owengpete@gmail.com>
2022-02-05 18:37:19 -0600

    add exit button to new channel popup

--- components/NewChannelPopup.tsx
+++ components/NewChannelPopup.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import toggleDimmer from '../utils/toggleDimmer';
-import { FiArrowRightCircle } from 'react-icons/fi';
+import { FiArrowRightCircle, FiX } from 'react-icons/fi';
 import { useRouter } from 'next/router';
 
 interface Props{
@@ -32,20 +32,26 @@ const NewChannelPopup = (props: Props) =>{
     <>
       {props.isSettingNewChannel && 
         <div className='new-channel-popup'>
-          <div className="new-channel-popup__input">
-            <input 
-              className='new-channel-popup__text-input'
-              type="text" 
-              placeholder='Channel Name'
-              value={channelName}
-              onChange={(e: any)=>setChannelName(e.target.value)}
-              onKeyDown={(e: any)=>e.key == 'Enter' ? handleChannelNameConfirm(channelName) : null}
-              autoFocus={true}
-          />
-          <FiArrowRightCircle 
-            className='new-channel-popup__confirm-icon'
-            onClick={(e: any)=>handleChannelNameConfirm(channelName)}
-          />
+          <div className='new-channel-popup__container'>
+            <FiX 
+              className='new-channel-popup__exit' 
+              onClick={()=>props.setIsSettingNewChannel(false)}
+            />
+            <div className="new-channel-popup__input">
+              <input 
+                className='new-channel-popup__text-input'
+                type="text" 
+                placeholder='Channel Name'
+                value={channelName}
+                onChange={(e: any)=>setChannelName(e.target.value)}
+                onKeyDown={(e: any)=>e.key == 'Enter' ? handleChannelNameConfirm(channelName) : null}
+                autoFocus={true}
+            />
+            <FiArrowRightCircle 
+              className='new-channel-popup__confirm-icon'
+              onClick={(e: any)=>handleChannelNameConfirm(channelName)}
+            />
+            </div>
           </div>
         </div>
       }
