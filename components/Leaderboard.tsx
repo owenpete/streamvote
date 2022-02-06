870c6c52c0107eb1a0792947a076ada105c2aa3d
Owen Peterson <owengpete@gmail.com>
2022-02-06 14:58:47 -0600

    add toggleable vote count/precentage on click

--- components/Leaderboard.tsx
+++ components/Leaderboard.tsx
@@ -7,6 +7,30 @@ interface Props{
 }
 
 const Leaderboard = (props: Props) =>{
+  const [isShowingVotes, setIsShowingVotes] = useState<boolean>(true);
+  const [totalVotes, setTotalVotes] = useState<number>(0);
+
+  useEffect(()=>{
+    let total = 0;
+    if(props.leaderboard != undefined){
+      props.leaderboard.map((value: any)=>{
+        if(value != undefined){
+          total += value.votes.length;
+        }
+      });
+    }
+    setTotalVotes(total);
+  });
+
+  const handleItemClick = () =>{
+    setIsShowingVotes(!isShowingVotes);
+  }
+
+  const getPrecentage = (votes: any, totalVotes: number): number =>{
+    const precentage = Math.round((votes/totalVotes) * 1000)/10;
+    return isNaN(precentage) ? 0 : precentage;
+  }
+
   return (
     <div 
       className="leaderboard"
@@ -18,12 +42,19 @@ const Leaderboard = (props: Props) =>{
               <li 
                 className='leaderboard__item'
                 key={index}
+                onClick={(e: any)=>handleItemClick()}
               >
                 <span className='item__number'>{index+1}.</span>{value.name}
                 {index == 0 && !props.isVoting &&
                   <FaCrown className='item__crown' />
                 }
-                <span className='item__votes'>{value.votes.length}</span>
+                <span className='item__votes'>
+                  {isShowingVotes?
+                      value.votes.length
+                    :
+                      getPrecentage(value.votes.length, totalVotes) + '%'
+                  }
+                </span>
               </li>
           )
         })
